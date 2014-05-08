3f512ed89c0fbd08ac60437ac297bbbf5350bf29
Tjark Saul <tjark.saul@wachplan.net>
2014-05-08 22:20:18 +0200

    Funktionen hinzugefügt, danke alias.sh

---
+++ .zsh/lib/functions.zsh
@@ -0,0 +1,46 @@
+extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+function github() {
+  local github_url
+ 
+  if ! git remote -v >/dev/null; then
+    return 1
+  fi
+ 
+  # get remotes for fetch
+  github_url="`git remote -v | grep github\.com | grep \(fetch\)$`"
+ 
+  if [ -z "$github_url" ]; then
+    echo "A GitHub remote was not found for this repository."
+    return 1
+  fi
+ 
+  # look for origin in remotes, use that if found, otherwise use first result
+  if [ "echo $github_url | grep '^origin' >/dev/null 2>&1" ]; then
+    github_url="`echo $github_url | grep '^origin'`"
+  else
+    github_url="`echo $github_url | head -n1`"
+  fi
+ 
+  github_url="`echo $github_url | awk '{ print $2 }' | sed 's/git@github\.com:/http:\/\/github\.com\//g'`"
+  echo $github_url
+}
