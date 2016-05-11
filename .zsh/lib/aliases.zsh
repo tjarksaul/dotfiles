5729a571441145bfffe39e245a90aabd38c57a45
Tjark Saul <tjark.saul@wachplan.net>
2016-05-11 15:35:10 +0200

    Fire

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -4,6 +4,7 @@ alias ...='cd ../..'
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias vinfo='mplayer -vo null -ao null -identify -frames 0'
 alias gut='git'
+alias fire='git commit -am "BURNING DOWN" && git push -f origin master'
 
 function mcd() {
     mkdir $1 && cd $1
