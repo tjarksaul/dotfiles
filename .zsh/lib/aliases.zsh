571578d145cee7275a7d9d336da861e904157d36
Tjark Saul <tjark.saul@wachplan.net>
2016-05-11 13:35:59 +0200

    gut push

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,3 +1,7 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+alias vinfo='mplayer -vo null -ao null -identify -frames 0'
+alias gut='git'
+
