d0eb097815c256bfc0451572f91b498336d37343
Tjark Saul <tjark.saul@wachplan.net>
2016-05-11 15:34:29 +0200

    Da fehlte was

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,7 +1,6 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-<<<<<<< HEAD
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias vinfo='mplayer -vo null -ao null -identify -frames 0'
 alias gut='git'
