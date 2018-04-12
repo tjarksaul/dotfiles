a5b610e72e3c62c1fc2b55a11054d0dd82444491
nichdu <tjark@saul.li>
2018-04-12 13:59:25 +0200

    Alias für xcodeproj-Generation

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -5,6 +5,7 @@ alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias vinfo='mplayer -vo null -ao null -identify -frames 0'
 alias gut='git'
 alias fire='git commit -am "BURNING DOWN" && git push -f origin master'
+alias xcp='swift package generate-xcodeproj | grep -o ".\/.*.xcodeproj$" | xargs open'
 
 function mcd() {
     mkdir $1 && cd $1
