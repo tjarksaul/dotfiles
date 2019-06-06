fad3b664b3f6503a7d9593133fd302c83bdd8ef4
nichdu <mail@appfel.dev>
2019-06-06 12:11:27 -0700

    Updated prompt with git

--- .zsh/lib/promt.zsh
+++ .zsh/lib/promt.zsh
@@ -1,25 +1,110 @@
-# autoload -U add-zsh-hook
+# Misc prompts
+ZCALCPROMPT='%F{48}%1v%f '
+SPROMPT='zsh: correct '%F{166}%R%f' to '%F{76}%r%f' [nyae]? '
+
+# Main prompt
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec _prompt_preexec
+add-zsh-hook precmd _prompt_precmd
+integer _prompt_next_command _prompt_last_command
+
+function _prompt_preexec() {
+	(( _prompt_next_command++ ))
+}
+
+function _prompt_precmd() {
+	setopt localoptions
+	local endsign host_color user_color last_was_empty
+	host_color="%F{${ZSH_HOST_COLOR:-33}}"
+
+	# Check if the last command was empty (always show that case as successful)
+	if (( _prompt_next_command == _prompt_last_command )); then
+		last_was_empty=1
+	else
+		last_was_empty=0
+		(( _prompt_last_command = _prompt_next_command ))
+	fi
+
+	# root gets a special prompt
+	if ((UID == 0)); then
+		endsign='#'
+		user_color='%F{9}'
+	else
+		endsign=''
+		user_color='%F{10}'
+	fi
+
+	# Highlight endsign if last command failed
+	if (( !last_was_empty )); then
+		endsign="%(?..%K{9}%F{15})$endsign%k"
+	fi
+
+	# Load VCS info
+	vcs_info
+
+	# Set the actual prompts
+	typeset -g PS1="%F{15}[$user_color%n%F{15}@$host_color%m%F{15} %F{11}%~%F{15}$vcs_info_msg_0_%F{15}]%F{14}$endsign%f "
+	typeset -g PS2='%F{14}%_%F{15}%f '
+}
+
+# VCS information
 autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' nvcsformats ''
+zstyle ':vcs_info:git*' check-for-changes true
+zstyle ':vcs_info:git*' check-for-staged-changes true
+zstyle ':vcs_info:git*' get-revision false
+zstyle ':vcs_info:git*' stagedstr '%F{2}+%f'
+zstyle ':vcs_info:git*' unstagedstr '%F{1}*%f'
+zstyle ':vcs_info:git*' formats ':%F{85}%b%F{85}%u%c%m%f'
+zstyle ':vcs_info:git*' actionformats ':%F{85}%b%F{85}%u%c%m%f|%F{67}%a%F{15}%f'
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-fancybranch git-stash git-space
 
-# # enable VCS systems you use
-zstyle ':vcs_info:*' enable git svn
-zstyle ':vcs_info:*:prompt:*' check-for-changes true
+function +vi-git-untracked() {
+	if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && git status --porcelain | grep '??' &> /dev/null ; then
+		hook_com[staged]+='%F{1}%%%f'
+	fi
+}
 
-setopt prompt_subst
-autoload -U promptinit
-promptinit
+function +vi-git-fancybranch() {
+	if [[ $hook_com[branch] == *... ]]; then
+		# get a nice branch name such as master~1
+		local fancy=$($vcs_comm[cmd] describe --contains --all HEAD 2>/dev/null)
+		if [[ -n $fancy ]]; then
+			hook_com[branch]="%F{1}($fancy)"
+		else
+			hook_com[branch]="%F{1}(${hook_com[branch]})"
+		fi
+	elif [[ $hook_com[branch] == remotes/* ]]; then
+		# for a remote we just need to add the () around it and colorize it
+		hook_com[branch]="%F{1}(${hook_com[branch]})"
+	elif [[ $hook_com[branch] == heads/* ]]; then
+		# for a head we let git show us the nice name like git-prompt does
+		hook_com[branch]="%F{1}($(${vcs_comm[cmd]} describe --contains --all HEAD))"
+	fi
+}
 
+function +vi-git-stash() {
+	local -a stashes
 
-# Look at http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information
-# for mor options
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '*'   # display this when there are unstaged changes
-zstyle ':vcs_info:*' stagedstr '+'  # display this when there are staged changes
-zstyle ':vcs_info:*' actionformats '[%b%|%a%c%u%]%f'
-zstyle ':vcs_info:*' formats ':%b%c%u%f'
+	hook_com[misc]=''  # we don't care about patches
+	if [[ -s ${hook_com[base]}/.git/refs/stash ]] ; then
+		stashes=$(git stash list 2>/dev/null | wc -l)
+		hook_com[misc]='%F{12}$'
+	fi
+}
 
+function +vi-git-space() {
+	# Terrible workaround because we can't use conditionals in the format string..
+	if [[ -n ${hook_com[unstaged]} || -n ${hook_com[staged]} || -n ${hook_com[misc]} ]]; then
+		hook_com[unstaged]=" ${hook_com[unstaged]}"
+	fi
+}
 
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
-precmd () { vcs_info }
-
-PROMPT='[%n@%m %~${vcs_info_msg_0_}] %{$reset_color%}'
+# adam2 prompt looks pretty but lacks stuff like vcs prompts
+# autoload -Uz promptinit
+# promptinit
+# prompt adam2 8bit
+# TRAPWINCH() {
+# 	zle && echo '' && prompt_adam2_precmd && zle reset-prompt
+# }
