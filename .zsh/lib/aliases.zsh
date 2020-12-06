0137279a3c414bb3e7ccbc54d945b9aef84567fe
Tjark Saul <mail@tjarksaul.de>
2020-12-06 14:39:24 +0100

    Small updates

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,7 +1,6 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-alias crontab='crontab -i'
 alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 alias vinfo='mplayer -vo null -ao null -identify -frames 0'
 alias gut='git'
